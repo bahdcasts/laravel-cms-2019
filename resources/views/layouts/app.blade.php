b82f9aadc3579f76c2bfcf43f7e5d34fdc48f270
Kati Frantz <bahdcoder@gmail.com>
2019-02-26 08:49:50 +0100

    add models for category and post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,27 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            @auth
+              <div class="container">
+                  <div class="row">
+                    <div class="col-md-4">
+                      <ul class="list-group">
+                        <li class="list-group-item">
+                          <a href="">Posts</a>
+                        </li>
+                        <li class="list-group-item">
+                          <a href="">Categories</a>
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="col-md-8">
+                        @yield('content')
+                    </div>
+                  </div>
+              </div>
+            @else
+              @yield('content')
+            @endauth
         </main>
     </div>
 </body>
