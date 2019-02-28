0257f84d59d1c3fac3c84bbcb917b557d96985da
Kati Frantz <bahdcoder@gmail.com>
2019-02-28 20:38:49 +0100

    add select 2

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -64,7 +64,7 @@
         <div class="form-group">
           <label for="tags">Tags</label>
               
-            <select name="tags[]" id="tags" class="form-control" multiple>
+            <select name="tags[]" id="tags" class="form-control tags-selector" multiple>
               @foreach($tags as $tag)
               <option value="{{ $tag->id }}"
                   @if(isset($post))
@@ -93,9 +93,14 @@
 @section('scripts')
   <script src="https://cdnjs.cloudflare.com/ajax/libs/trix/1.0.0/trix.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
   <script>
     flatpickr('#published_at', {
       enableTime: true
+    })
+
+    $(document).ready(function() {
+      $('.tags-selector').select2();
     })
   </script>
 @endsection
@@ -103,4 +108,5 @@
 @section('css')
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/trix/1.0.0/trix.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 @endsection
