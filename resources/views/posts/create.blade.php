760386ce09db0ab8178c60d488acd4ced880cf51
edolorenza <48113050+edolorenza@users.noreply.github.com>
2019-08-09 08:02:00 +0700

    fix select when updating category

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -45,7 +45,7 @@
 
       <div class="form-group">
         <label for="category">Category</label>
-        <select name="category" id="category" class="form-control">
+        <select name="category_id" id="category_id" class="form-control">
           @foreach($categories as $category)
             <option value="{{ $category->id }}"
                 @if(isset($post))
